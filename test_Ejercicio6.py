b6b0a7e6c2a5ba4e978d738831279a7521a252f1
alejandro <luis.vejarano01@correo.usa.edu.co>
2020-09-10 02:31:20 -0500

    Agrego función crecimiento

--- test_Ejercicio6.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-
-from Ejercicio6 import palindroma
-
-
-class Test(TestCase):
-    def test_palindroma(self):
-        self.assertEqual(True,palindroma("Ana"))
-        self.assertEqual(False,palindroma("Casa"))
